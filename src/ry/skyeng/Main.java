2d616ab67807dcef927d0c3ae0d2a466ad456b6c
Eremeev Family <erema_paren@mail.ru>
2021-10-13 11:03:29 +0300

    Что сделал.

---
+++ src/ry/skyeng/Main.java
@@ -0,0 +1,25 @@
+package ry.skyeng;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte apple = 127;
+        System.out.println(apple);
+
+        short orange = 12443;
+        System.out.println(orange);
+
+        int egg = 123344432;
+        System.out.println(egg);
+
+        long pen = 1343464564;
+        System.out.println(pen + egg + orange + apple);
+
+        float egg3 = 5.55f;
+        System.out.println("В корзине"egg3 );
+        double egg2 = 5.055d;
+        var box = egg2+egg3
+        System.out.println("В корзине"box);
+    }
